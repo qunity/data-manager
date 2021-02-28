7175d30704ae328d9b9ab317b5ace2792937a6b6
Rodion Kachkin <kyleRQWS@gmail.com>
2021-03-01 00:19:28 +0400

    Refactor autoload tests file

--- test/autoload.php
+++ test/autoload.php
@@ -9,11 +9,11 @@
  * file that was distributed with this source code.
  */
 
-require __DIR__ . '/DataManager/Helper/Converter/Provider.php';
-require __DIR__ . '/DataManager/Helper/Recursive/Provider.php';
-require __DIR__ . '/DataManager/Helper/Recursive/AnotherDataManager.php';
-require __DIR__ . '/DataManagerFactory/AnotherDataManager.php';
-require __DIR__ . '/DataManagerFactory/Provider.php';
+require __DIR__ . '/AbstractDataManager/AnotherDataManager.php';
 require __DIR__ . '/AbstractDataManager/Helper.php';
 require __DIR__ . '/AbstractDataManager/Provider.php';
-require __DIR__ . '/AbstractDataManager/AnotherDataManager.php';
+require __DIR__ . '/DataManager/Helper/Converter/Provider.php';
+require __DIR__ . '/DataManager/Helper/Recursive/AnotherDataManager.php';
+require __DIR__ . '/DataManager/Helper/Recursive/Provider.php';
+require __DIR__ . '/DataManagerFactory/AnotherDataManager.php';
+require __DIR__ . '/DataManagerFactory/Provider.php';
