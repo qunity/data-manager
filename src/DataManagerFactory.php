137308c6cec6b7891bb6d470243fe176f7b437ca
Rodion Kachkin <kyleRQWS@gmail.com>
2021-03-31 09:17:23 +0400

    Fix phpstan errors in DataManagerFactory

--- src/DataManagerFactory.php
+++ src/DataManagerFactory.php
@@ -24,7 +24,7 @@ class DataManagerFactory
     /**
      * Create new manager object
      *
-     * @param array $data
+     * @param array<mixed,mixed> $data
      * @param string|null $class
      *
      * @return DataManagerInterface
