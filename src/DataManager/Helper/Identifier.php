a1a0b8b59b4684542d3022a03530f681bf290732
kyleRQWS <kyleRQWS@gmail.com>
2021-05-22 07:49:20 +0400

    Refactor Identifier helper

--- src/DataManager/Helper/Identifier.php
+++ src/DataManager/Helper/Identifier.php
@@ -19,6 +19,8 @@ use Qunity\Component\DataManagerInterface;
 /**
  * Class Identifier
  * @package Qunity\Component\DataManager\Helper
+ *
+ * @SuppressWarnings(PHPMD.ShortVariable)
  */
 class Identifier
 {
@@ -36,7 +38,6 @@ class Identifier
 
     /**
      * Get keys by identifier
-     * @SuppressWarnings(PHPMD.ShortVariable)
      *
      * @param int|string $id
      * @return array<int,string>
@@ -46,11 +47,10 @@ class Identifier
         if ($id !== '') {
             if (isset(self::$keys[$id])) {
                 return self::$keys[$id];
-            } else {
-                return self::$keys[$id] = array_reverse(
-                    explode(DataManagerInterface::DELIMITER_PATH, (string)$id)
-                );
             }
+            return self::$keys[$id] = array_reverse(
+                explode(DataManagerInterface::DELIMITER_PATH, (string)$id)
+            );
         }
         return [];
     }
@@ -71,29 +71,25 @@ class Identifier
             }
             if (isset(self::$underscore[$method])) {
                 return self::$underscore[$method];
-            } else {
-                return self::$underscore[$method] = (string)preg_replace([
-                    '%' . DataManagerInterface::DELIMITER_KEY . '{2,}%',
-                    '%[^a-z0-9]*' . DataManagerInterface::DELIMITER_PATH . '+[^a-z0-9]*%',
-                ], [
-                    DataManagerInterface::DELIMITER_KEY,
-                    DataManagerInterface::DELIMITER_PATH,
-                ], strtolower(trim(
-                    (string)preg_replace(
-                        ['%' . DataManagerInterface::DELIMITER_KEY . '+%', '%([A-Z]|[0-9]+)%'],
-                        [DataManagerInterface::DELIMITER_PATH, DataManagerInterface::DELIMITER_KEY . '\\1'],
-                        $method
-                    ),
-                    DataManagerInterface::DELIMITER_KEY . DataManagerInterface::DELIMITER_PATH
-                )));
             }
+            return self::$underscore[$method] = strtolower(trim(
+                (string)preg_replace([
+                    '%' . DataManagerInterface::DELIMITER_KEY . '+%',
+                    '%([A-Z]|[0-9]+)%',
+                    '%[^A-Za-z0-9]*' . DataManagerInterface::DELIMITER_PATH . '+[^A-Za-z0-9]*%',
+                ], [
+                    DataManagerInterface::DELIMITER_PATH,
+                    DataManagerInterface::DELIMITER_KEY . '\\1',
+                    DataManagerInterface::DELIMITER_PATH,
+                ], $method),
+                DataManagerInterface::DELIMITER_KEY . DataManagerInterface::DELIMITER_PATH
+            ));
         }
         return '';
     }
 
     /**
      * Check if identifier is path
-     * @SuppressWarnings(PHPMD.ShortVariable)
      *
      * @param int|string $id
      * @param bool|null $throw
@@ -105,7 +101,7 @@ class Identifier
         $result = str_contains((string)$id, DataManagerInterface::DELIMITER_PATH);
         if ($throw !== null && $throw == $result) {
             throw new InvalidArgumentException(sprintf(
-                "Argument must be of the form '%s', given argument is be '%s': %s",
+                "Argument must be of the form '%s', given argument is be '%s' (%s)",
                 ...($throw ? ['key', 'path', $id] : ['path', 'key', $id])
             ));
         }
