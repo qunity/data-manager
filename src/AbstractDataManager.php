af55eb74f5def769a971348c0fa32fd550edddf1
kyleRQWS <kyleRQWS@gmail.com>
2021-06-03 06:12:17 +0400

    Add TODO "add key argument to callback function" in AbstractDataManager

--- src/AbstractDataManager.php
+++ src/AbstractDataManager.php
@@ -102,7 +102,8 @@ abstract class AbstractDataManager implements DataManagerInterface
                 return Recursive::try(Identifier::getKeys($id), $this->data, $check);
             } elseif (key_exists($id, $this->data)) {
                 if ($check !== null) {
-                    return call_user_func($check, $this->data[$id]);
+                    // TODO: add key argument to callback function
+                    return (bool)call_user_func($check, $this->data[$id]);
                 }
                 return true;
             }
